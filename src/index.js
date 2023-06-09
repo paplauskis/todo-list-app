44cc78bde97e8f3a2b117e03d725f1d96480df8f
paplauskis <paplauskaskostas@gmail.com>
2023-06-09 17:28:16 +0300

    added 2 functions to control app flow, 2 buttons to display forms

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { controlForms } from "./form-control";
+import { addNote } from "./create-note";
+import { controlSections } from "./control-sections";
+import { addTodo } from "./create-todo";
+
+
+controlForms();
+controlSections();
+
+const submitNoteFormButton = document.querySelector('.add-new-note');
+submitNoteFormButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  addNote();
+})
+
+const submitTodoFormButton = document.querySelector('.add-new-todo');
+submitTodoFormButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  addTodo();
+})
