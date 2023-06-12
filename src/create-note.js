f376b9de90d04b958cd3229e375ea7a1161dfb98
paplauskis <paplauskaskostas@gmail.com>
2023-06-12 20:47:17 +0300

    removed noteArrays and everything related to them, no need for them

--- src/create-note.js
+++ src/create-note.js
@@ -1,6 +1,5 @@
 import { notesSection } from "./control-sections";
 
-let notesArray = [];
 const noteDescription = document.querySelector('#note');
 const noteForm = document.querySelector('#note-form');
 
@@ -13,30 +12,21 @@ class Note {
 export function addNote() {
   const newNote = new Note(noteDescription.value.trim());
   if (noteDescription.value.trim().length > 0) {
-    notesArray.push(newNote);
     displayNote(newNote);
     noteForm.reset();
   } 
 }
 
 function displayNote(note) {
-  const noteArrayDiv = document.createElement('div');
-  noteArrayDiv.classList.add('note-array-div');
-  noteArrayDiv.textContent = `${note.description}`;
+  const noteDiv = document.createElement('div');
+  noteDiv.classList.add('note-array-div');
+  noteDiv.textContent = `${note.description}`;
   const removeNoteButton = document.createElement('button');
   removeNoteButton.textContent = '✕';
   removeNoteButton.classList.add('remove-note-button');
   removeNoteButton.addEventListener('click', () => {
-    noteArrayDiv.remove();
-    removeNoteFromArray(note);
+    noteDiv.remove();
   });
-  noteArrayDiv.appendChild(removeNoteButton);
-  notesSection.appendChild(noteArrayDiv);
+  noteDiv.appendChild(removeNoteButton);
+  notesSection.appendChild(noteDiv);
 }
-
-function removeNoteFromArray(note) {
-  const index = notesArray.indexOf(note);
-  if (index !== -1) notesArray.splice(index, 1);
-}
-
-
