9797e4303afd57fd50dba79f5f6df29ffbe68da8
paplauskis <paplauskaskostas@gmail.com>
2023-06-12 20:45:36 +0300

    changed symbol from todoArrayDiv to todoDiv

--- src/control-sections.js
+++ src/control-sections.js
@@ -2,6 +2,7 @@ export const allTodosSection = document.querySelector('.all-todos-section')
 export const completedTodosSection = document.querySelector('.completed-todos-section')
 export const notesSection = document.querySelector('.notes-section')
 
+
 const allTodosSectionButton = document.querySelector('.all-todos-section-button');
 const todayTodosSectionButton = document.querySelector('.today-todos-section-button');
 const tommorowTodosSectionButton = document.querySelector('.tommorow-todos-section-button');
@@ -9,8 +10,13 @@ const lowPriorityTodosSectionButton = document.querySelector('.low-priority-sect
 const mediumPriorityTodosSectionButton = document.querySelector('.medium-priority-section-button');
 const highPriorityTodosSectionButton = document.querySelector('.high-priority-section-button');
 
-export function controlSections() {
+export const title = document.querySelector('#title');
+export const todoDescription = document.querySelector('#description');
+export const dueDate = document.querySelector('#due-date');
+export const priority = document.querySelector('#select-priority');
+export const todoForm = document.querySelector('#todo-form')
 
+export function controlSections() {
   allTodosSection.style.display = 'flex';
   completedTodosSection.style.display = 'none';
   notesSection.style.display = 'none';
@@ -22,29 +28,28 @@ export function controlSections() {
       completedTodosSection.style.display = 'none';
   });
 
-  const allTodosSectionButton = document.querySelector('.all-todos-section-button');
+  
   allTodosSectionButton.addEventListener('click', () => {
       allTodosSection.style.display = 'flex';
       completedTodosSection.style.display = 'none';
       notesSection.style.display = 'none';
-
-  });
-
-  const completedTodosSectionButton = document.querySelector('.completed-todos-section-button');
-  completedTodosSectionButton.addEventListener('click', () => {
+    });
+    
+    const completedTodosSectionButton = document.querySelector('.completed-todos-section-button');
+    completedTodosSectionButton.addEventListener('click', () => {
     allTodosSection.style.display = 'none';
     completedTodosSection.style.display = 'flex';
     notesSection.style.display = 'none';
   });
 }
 
-export function sectionLoader(todoArrayDiv) {
-  lowPriorityTodosSectionButton.addEventListener('click', () => loadLowPrioritySection(todoArrayDiv))
-  mediumPriorityTodosSectionButton.addEventListener('click', () => loadMediumPrioritySection(todoArrayDiv))
-  highPriorityTodosSectionButton.addEventListener('click', () => loadHighPrioritySection(todoArrayDiv))
-  allTodosSectionButton.addEventListener('click', () => loadAllTodosSection(todoArrayDiv));
-  todayTodosSectionButton.addEventListener('click', () => loadTodayTodosSection(todoArrayDiv));
-  tommorowTodosSectionButton.addEventListener('click', () => loadTommorowTodosSection(todoArrayDiv))
+export function sectionLoader(todoDiv) {
+  lowPriorityTodosSectionButton.addEventListener('click', () => loadLowPrioritySection(todoDiv))
+  mediumPriorityTodosSectionButton.addEventListener('click', () => loadMediumPrioritySection(todoDiv))
+  highPriorityTodosSectionButton.addEventListener('click', () => loadHighPrioritySection(todoDiv))
+  allTodosSectionButton.addEventListener('click', () => loadAllTodosSection(todoDiv));
+  todayTodosSectionButton.addEventListener('click', () => loadTodayTodosSection(todoDiv));
+  tommorowTodosSectionButton.addEventListener('click', () => loadTommorowTodosSection(todoDiv))
 }
 
 function hideNotesAndCompletedTodos() {
@@ -53,43 +58,43 @@ function hideNotesAndCompletedTodos() {
   notesSection.style.display = 'none';
 }
 
-export function loadAllTodosSection(todoArrayDiv) {
-  todoArrayDiv.style.display = 'flex';
+export function loadAllTodosSection(todoDiv) {
+  todoDiv.style.display = 'flex';
   completedTodosSection.style.display = 'none';
   notesSection.style.display = 'none';
 }
 
-export function loadLowPrioritySection(todoArrayDiv) {
-  if (todoArrayDiv.classList[2] == 'low') {
-    todoArrayDiv.style.display = 'flex'
-  } else todoArrayDiv.style.display = 'none'
+export function loadLowPrioritySection(todoDiv) {
+  if (todoDiv.classList[2] == 'low') {
+    todoDiv.style.display = 'flex'
+  } else todoDiv.style.display = 'none'
   hideNotesAndCompletedTodos()
 }
 
-export function loadMediumPrioritySection(todoArrayDiv) {
-  if (todoArrayDiv.classList[2] == 'medium') {
-    todoArrayDiv.style.display = 'flex'
-  } else todoArrayDiv.style.display = 'none'
+export function loadMediumPrioritySection(todoDiv) {
+  if (todoDiv.classList[2] == 'medium') {
+    todoDiv.style.display = 'flex'
+  } else todoDiv.style.display = 'none'
   hideNotesAndCompletedTodos()
 }
 
-export function loadHighPrioritySection(todoArrayDiv) {
-  if (todoArrayDiv.classList[2] == 'high') {
-    todoArrayDiv.style.display = 'flex'
-  } else todoArrayDiv.style.display = 'none'
+export function loadHighPrioritySection(todoDiv) {
+  if (todoDiv.classList[2] == 'high') {
+    todoDiv.style.display = 'flex'
+  } else todoDiv.style.display = 'none'
   hideNotesAndCompletedTodos()
 }
 
-export function loadTodayTodosSection(todoArrayDiv) {
-  if (todoArrayDiv.classList[1] == 'today') {
-    todoArrayDiv.style.display = 'flex'
-  } else todoArrayDiv.style.display = 'none'
+export function loadTodayTodosSection(todoDiv) {
+  if (todoDiv.classList[1] == 'today') {
+    todoDiv.style.display = 'flex'
+  } else todoDiv.style.display = 'none'
   hideNotesAndCompletedTodos()
 }
 
-export function loadTommorowTodosSection(todoArrayDiv) {
-  if (todoArrayDiv.classList[1] == 'tommorow') {
-    todoArrayDiv.style.display = 'flex'
-  } else todoArrayDiv.style.display = 'none'
+export function loadTommorowTodosSection(todoDiv) {
+  if (todoDiv.classList[1] == 'tommorow') {
+    todoDiv.style.display = 'flex'
+  } else todoDiv.style.display = 'none'
   hideNotesAndCompletedTodos()
 }
