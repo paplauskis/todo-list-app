5948fae4a27a6b10dd2d52be14e345cd6f78164f
paplauskis <paplauskaskostas@gmail.com>
2023-06-13 21:20:04 +0300

    made functions to save and display todos with local storage
    but didn't manage to delete todos from local storage on button click, and sort to complete, will try again later

---
+++ src/local-storage.js
@@ -0,0 +1,21 @@
+// import { createTodoDiv } from "./create-todo";
+
+// export function addTodoToLocalStorage(todo) {
+//   const existingTodos = JSON.parse(localStorage.getItem('todos')) || [];
+//   existingTodos.push(todo);
+//   localStorage.setItem('todos', JSON.stringify(existingTodos));
+//   return { todo }
+// }
+
+// export function displayTodosFromLocalStorage() {
+//   const todos = JSON.parse(localStorage.getItem('todos')) || [];
+//   todos.forEach(todo => {
+//     createTodoDiv(todo);
+//   });
+// }
+
+// export function removeTodoFromLocalStorage(todo) {
+//   const todos = JSON.parse(localStorage.getItem('todos')) || [];
+//   const index = todos.indexOf(todo)
+//   if (index != -1) todos.splice(index, 1);
+// }
